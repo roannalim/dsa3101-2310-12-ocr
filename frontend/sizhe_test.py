be977f9c9cc51d689a0d161841f73ec75183bf01
Sun Sizhe <e0774335@u.nus.edu>
2023-10-23 15:02:05 +0800

    Create /static folder to store images, edit the Upload.html and login.html to use image in the /static folder

--- frontend/sizhe_test.py
+++ frontend/sizhe_test.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('Main.html')
+    return render_template('upload.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
