0970e77a509f488db016b9ff68817f568c61449a
Sun Sizhe <e0774335@u.nus.edu>
2023-10-26 20:43:34 +0800

    Add history.html page, history_icon_png. Modify the Main_html page to connect to history.html

--- frontend/sizhe_test.py
+++ frontend/sizhe_test.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import os
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploaded_images'
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok = True)
 
 @app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/home')
 def main():
     return render_template('Main.html')
+
+@app.route('/image_processing', methods = ['POST'])
+def image_processing():
+    if 'image' in request.files:
+        image = request.files['image']
+
+        if image.filename != '':
+            curr_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
+            filename = curr_datetime + '.jpg'
+            image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('image_preview', filename = filename))
+        
+    return "Image processing failed"
+
+@app.route('/image_preview/<filename>')
+def image_preview(filename):
+    return render_template('Image_Preview.html', filename = filename)
 
 @app.route('/dashboard')
 def dashboard():
@@ -13,6 +38,11 @@ def dashboard():
 @app.route('/thank_you')
 def thank_you():
     return render_template('Thank_you.html')
+
+@app.route('/history')
+def history():
+    return render_template('history.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
