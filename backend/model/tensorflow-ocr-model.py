cbea1e824833b281d84a25f86a2a1ce68178ccba
Roanna <lim.roanna@gmail.com>
2023-11-11 17:28:48 +0800

    Updated tensorflow model with object detection training done.

--- backend/model/tensorflow-ocr-model.py
+++ backend/model/tensorflow-ocr-model.py
@@ -3,9 +3,17 @@ import os
 import wget
 import git
 import object_detection
+import cv2
+import numpy as np
+
+from matplotlib import pyplot as plt
 from object_detection.utils import config_util
 from object_detection.protos import pipeline_pb2
 from google.protobuf import text_format
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.builders import model_builder
+from object_detection.utils import config_util
 
 # Setting up paths for model 
 CUSTOM_MODEL_NAME = 'tensorflow_rcnn_model'
@@ -103,8 +111,76 @@ with tf.io.gfile.GFile(files['PIPELINE_CONFIG'], "wb") as f:
     f.write(config_text)
     
     
-# Train the model 
+""" # Train the model 
 TRAINING_SCRIPT = os.path.join(paths['APIMODEL_PATH'], 'research', 'object_detection', 'model_main_tf2.py')
 command = "python {} --model_dir={} --pipeline_config_path={} --num_train_steps=10000".format(TRAINING_SCRIPT, paths['CHECKPOINT_PATH'],files['PIPELINE_CONFIG'])
 print(command)
-os.system(command)
+os.system(command) """
+
+# Evaluate the model
+""" TRAINING_SCRIPT = os.path.join(paths['APIMODEL_PATH'], 'research', 'object_detection', 'model_main_tf2.py')
+command = "python {} --model_dir={} --pipeline_config_path={} --checkpoint_dir={}".format(TRAINING_SCRIPT, paths['CHECKPOINT_PATH'],files['PIPELINE_CONFIG'], paths['CHECKPOINT_PATH'])
+print(command)
+os.system(command) """
+
+# Load pipeline config and build a detection model
+configs = config_util.get_configs_from_pipeline_file(files['PIPELINE_CONFIG'])
+detection_model = model_builder.build(model_config=configs['model'], is_training=False)
+
+# Restore checkpoint
+ckpt = tf.compat.v2.train.Checkpoint(model=detection_model) 
+ckpt.restore(os.path.join(paths['CHECKPOINT_PATH'], 'ckpt-11')).expect_partial()
+
+@tf.function
+def detect_fn(image):
+    image, shapes = detection_model.preprocess(image)
+    prediction_dict = detection_model.predict(image, shapes)
+    detections = detection_model.postprocess(prediction_dict, shapes)
+    return detections
+
+# Detectiong from an image
+category_index = label_map_util.create_category_index_from_labelmap(files['LABELMAP'])
+IMAGE_PATH = os.path.join(paths['IMAGE_PATH'], 'test', '20231006_083526.jpg')
+img = cv2.imread(IMAGE_PATH)
+#image_np = dict(enumerate(np.array(img).flatten(), 1))
+image_np = np.array(img)
+#print(image_np)
+
+input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
+detections = detect_fn(input_tensor)
+
+num_detections = int(detections.pop('num_detections'))
+detections = {key: value[0, :num_detections].numpy()
+              for key, value in detections.items()}
+detections['num_detections'] = num_detections
+
+# detection_classes should be ints.
+detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+label_id_offset = 1
+#image_np_with_detections = dict(enumerate(np.array(img).flatten(), 1)).copy()
+#image_np_with_detections_str = ''.join(map(str, image_np_with_detections))
+#image_np_with_detections_int = int(image_np_with_detections_str)
+image_np_with_detections = image_np.copy()
+
+viz_utils.visualize_boxes_and_labels_on_image_array(
+            #image_np_with_detections_int,
+            image_np_with_detections,
+            detections['detection_boxes'],
+            detections['detection_classes']+label_id_offset,
+            detections['detection_scores'],
+            category_index,
+            use_normalized_coordinates=True,
+            max_boxes_to_draw=5,
+            min_score_thresh=.8,
+            agnostic_mode=False)
+
+plt.imshow(cv2.cvtColor(image_np_with_detections, cv2.COLOR_BGR2RGB))
+#plt.imshow(cv2.imread("Show image @ line 171", cv2.cvtColor(image_np_with_detections, cv2.COLOR_BGR2RGB)))
+plt.show()
+#cv2.imshow("Show image @ line 171", cv2.cvtColor(image_np_with_detections, cv2.COLOR_BGR2RGB))
+#cv2.waitKey(0)
+
+detections.keys()
+
+print('heelo world')
